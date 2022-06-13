e9a9a71fc9d95a15cda3a5e49a56351ed9d6bd97
wojas666 <wardak093@gmail.com>
2022-06-13 13:01:20 +0200

    ...

---
+++ Wardakstudio.Services.ProductsAPI.UnitTests/Mocks/MockProductRepository.cs
@@ -0,0 +1,62 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wardakstudio.Services.ProductsAPI.Models;
+using Wardakstudio.Services.ProductsAPI.Repository.Contracts;
+
+namespace Wardakstudio.Services.ProductsAPI.UnitTests.Mocks
+{
+    public static class MockProductRepository
+    {
+        public static Mock<IProductRepository> GetProductRepository()
+        {
+            var products = new List<Product>
+            {
+                new Product()
+                {
+                    Id = 1,
+                    Price = 5m,
+                    DateCreated = DateTime.Now,
+                    Name = "Pierwszy produkt",
+                    IsPublished = true,
+                    Description = "Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.",
+                    AvailableQuantity = 5
+                },
+                new Product()
+                {
+                    Id = 2,
+                    Price = 18m,
+                    DateCreated = DateTime.Now,
+                    Name = "Drugi produkt",
+                    IsPublished = true,
+                    Description = "Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.",
+                    AvailableQuantity = 8
+                },
+                new Product()
+                {
+                    Id = 3,
+                    Price = 65m,
+                    DateCreated = DateTime.Now,
+                    Name = "Trzeci produkt",
+                    IsPublished = true,
+                    Description = "Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.Lorem ipsum dolor.",
+                    AvailableQuantity = 5
+                }
+            };
+
+            var mockRepository = new Mock<IProductRepository>();
+
+            mockRepository.Setup(x => x.GetAll()).ReturnsAsync(products);
+            mockRepository.Setup(x => x.GetById(It.IsAny<int>())).ReturnsAsync((int id) =>
+            {
+                var product = products.Find(x => x.Id == id);
+                return product;
+            });
+
+            return mockRepository;
+        }
+    }
+}
---
+++ Wardakstudio.Services.ProductsAPI.UnitTests/Producers/Commands/DeleteProducerCommandHandlerTests.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wardakstudio.Services.ProductsAPI.Exceptions;
+using Wardakstudio.Services.ProductsAPI.Features.Producers.Handlers.Commands;
+using Wardakstudio.Services.ProductsAPI.Features.Producers.Requests.Commands;
+using Wardakstudio.Services.ProductsAPI.Models;
+using Wardakstudio.Services.ProductsAPI.Profiles;
+using Wardakstudio.Services.ProductsAPI.Repository.Contracts;
+using Wardakstudio.Services.ProductsAPI.UnitTests.Mocks;
+
+namespace Wardakstudio.Services.ProductsAPI.UnitTests.Producers.Commands
+{
+    public class DeleteProducerCommandHandlerTests
+    {
+        private readonly Mock<IProducerRepository> _mockRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteProducerCommandHandlerTests()
+        {
+            _mockRepository = MockProducerRepository.GetProducerRepository();
+
+            var mapperConfiguration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfiguration.CreateMapper();
+        }
+
+        [Fact]
+        public async Task If_producerid_exist_in_producer_repository_Shuld_run_delete_method_in_repository()
+        {
+            var handler = new DeleteProducerCommandHandler(_mockRepository.Object, _mapper);
+            await handler.Handle(new DeleteProducerCommand() { ProducerId = 1 }, CancellationToken.None);
+
+            _mockRepository.Verify(x => x.Delete(It.IsAny<Producer>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task If_producerid_not_exist_in_producer_repository_Should_return_notfounexception()
+        {
+            var handler = new DeleteProducerCommandHandler(_mockRepository.Object, _mapper);
+            await Should.ThrowAsync<NotFoundException>(async () => await handler.Handle(new DeleteProducerCommand() { ProducerId = Int32.MaxValue }, CancellationToken.None));
+        }
+    }
+}
